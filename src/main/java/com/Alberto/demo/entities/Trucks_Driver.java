3ed0b7ece5c5276eb0263bb67711de37bd5e0b77
storm-clash <58226583+storm-clash@users.noreply.github.com>
2023-08-31 20:46:40 -0700

    Madrugada

--- src/main/java/com/Alberto/demo/entities/Trucks_Driver.java
+++ src/main/java/com/Alberto/demo/entities/Trucks_Driver.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-//@Table(name = "trucks_driver")
+@Table(name = "trucks_driver")
 public class Trucks_Driver {
 
     @EmbeddedId
